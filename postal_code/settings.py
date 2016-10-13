6d0049a1018a51a0d6ba7a66114091a188f4a4ee
xecgr <francesc.garcia.robert@gmail.com>
2016-10-13 05:46:50 +0200

    Heroku: Static files -> https://devcenter.heroku.com/articles/django-assets

--- postal_code/settings.py
+++ postal_code/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 NOMINATIM_KEY        = 'yqkNdcTDr0acMPfcVKfkx7MBhoHSQSPg'
 DEFAULT_COUNTRY_CODE = u'ES'
